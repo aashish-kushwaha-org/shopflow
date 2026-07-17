9416343cc1f53fe633f4f830c64160161ee5b0b1
Aashish Kushwaha <48587325+Aashishkushwaha@users.noreply.github.com>
2026-07-17 17:23:20 +0530

    feat(setup): install required dependency (#27)
    - setup eslint & prettier
    - setup husky and commitlint
    - setup PULL_REQUEST_TEMPLATE.md file

--- eslint.config.js
+++ eslint.config.js
@@ -4,19 +4,24 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'error',
+        },
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+            eslintConfigPrettier,
+        ],
+        languageOptions: {
+            globals: globals.browser,
+        },
     },
-  },
 ])
