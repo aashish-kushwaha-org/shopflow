b1da328f39389d301b1e7798732ac48221206c1c
msdaashish <msdaashish@gmail.com>
2026-07-24 19:05:14 +0530

    feat(cart): create cart page, add functionlity to update the quantity of item added to cart

---
+++ src/features/cart/CartPage.tsx
@@ -0,0 +1,87 @@
+import { Link } from 'react-router-dom';
+import { List } from '@/components/List';
+import { useAuth } from '../auth/useAuth';
+import { useCart } from './useCart';
+import { calculateCartTotal, getCartItemsCount } from './utils';
+import { formatPrice } from '@/lib/price';
+import type { CartItem } from '@/types/product.types';
+
+const CartItem = ({ name, price, quantity, imageUrl, productId }: CartItem) => {
+    const { dispatch } = useCart();
+
+    const updateQuantityHandler = (quantity: number) => {
+        dispatch({ type: 'UPDATE_QUANTITY', payload: { productId, quantity } });
+    };
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                padding: 4,
+                marginInline: 12,
+                gap: 32,
+                marginBlockEnd: 4,
+                border: '2px solid black',
+            }}
+        >
+            <img
+                style={{ maxWidth: 120, aspectRatio: '16/6' }}
+                src={imageUrl}
+                alt={name}
+            />
+            <div>
+                <h3>
+                    <Link
+                        style={{ color: 'currentColor' }}
+                        to={`/product/${productId}`}
+                    >
+                        {name}
+                    </Link>
+                </h3>
+                <p>Price: {formatPrice(price)}</p>
+                <p>Quantity: {quantity}</p>
+                <br />
+                <p>Total: {formatPrice(price * quantity)}</p>
+                <div style={{ display: 'flex', gap: 8 }}>
+                    <button
+                        disabled={quantity === 1}
+                        onClick={() => updateQuantityHandler(quantity - 1)}
+                    >
+                        Remove 1 item
+                    </button>
+                    <button onClick={() => updateQuantityHandler(quantity + 1)}>
+                        Add 1 item
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const CartPage = () => {
+    const { user } = useAuth();
+    const { state: cartState } = useCart();
+
+    return (
+        <div>
+            <h1>Hi {user?.name},</h1>
+            <p>
+                You've total ({getCartItemsCount(cartState.items)}) items in the
+                cart.
+            </p>
+            <br />
+            <h3>Cart Items</h3>
+            <List
+                items={cartState.items}
+                keyExtractor={(item) => item.productId}
+                renderItem={CartItem}
+            />
+            <hr />
+            <h4>
+                Cart Total: {formatPrice(calculateCartTotal(cartState.items))}
+            </h4>
+        </div>
+    );
+};
+
+export default CartPage;
